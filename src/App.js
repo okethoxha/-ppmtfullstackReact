2ec4fedd5c9f4483017e521671c1c1a03473f15f
Oket <oket.hoxha@gmail.com>
2021-04-15 00:04:03 -0400

    first react component added!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to PPMT</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-      </header>
+      <Dashboard />
     </div>
   );
 }
