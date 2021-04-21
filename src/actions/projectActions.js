2cf660484025c2d6e07fc4501810100831c8b2cd
Oket <oket.hoxha@gmail.com>
2021-04-20 22:24:14 -0400

    UPdate Bug function done.

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -4,7 +4,14 @@ import {GET_ERRORS, GET_PROJECT, GET_PROJECTS} from "./types";
 export const createProject = (project, history) => async dispatch => {
     try{
         const res = await axios.post("http://localhost:8081/api/project", project)
-        history.push("/dashboard")
+        history.push("/dashboard");
+
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        })
+
+
     }catch (err){
         dispatch({
             type: GET_ERRORS,
