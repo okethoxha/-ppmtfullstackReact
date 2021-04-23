0abb9a147591201492e1db5834d9abcd3fb3a719
Oket <oket.hoxha@gmail.com>
2021-04-23 15:18:29 -0400

    Types and reducers for PTs

---
+++ src/reducers/backlogReducer.js
@@ -0,0 +1,34 @@
+import {GET_BACKLOG, GET_PROJECT_TASK, DELETE_PROJECT_TASK} from "../actions/types";
+import {act} from "react-dom/test-utils";
+
+const initState = {
+    project_tasks: [],
+    project_task: {}
+}
+
+export default function (state = initState, action) {
+    switch (action.type){
+
+        case GET_BACKLOG:
+            return {
+                ...state,
+                project_tasks: action.payload
+            }
+
+        case GET_PROJECT_TASK:
+            return {
+                ...state,
+                project_task: action.payload
+            }
+
+        case DELETE_PROJECT_TASK:
+            return {
+                ...state
+                //TODO
+            }
+
+
+        default:
+            return state;
+    }
+}
