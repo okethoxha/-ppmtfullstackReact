5bb3d70e3bf7ace69e31f87a43d1fb7dde10413e
Oket <oket.hoxha@gmail.com>
2021-05-17 23:07:28 +0200

    Security Reducer and Actions added.

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,7 +4,6 @@ export const GET_PROJECT = "GET_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
 
 //Types for BACKLOG ACTIONS
-
 export const GET_BACKLOG = "GET_BACKLOG";
 export const GET_PROJECT_TASK = "GET_PROJECT_TASK";
 export const DELETE_PROJECT_TASK = "DELETE_PROJECT_TASK";
